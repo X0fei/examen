10096800e47d437dfb6612b92778cee530b531c9
Xofei <pitonvano05@gmail.com>
2024-12-23 12:40:09 +0300

    group window

--- PresenceDesktop/MainWindow.axaml.cs
+++ PresenceDesktop/MainWindow.axaml.cs
@@ -12,8 +12,9 @@ namespace PresenceDesktop
     {
         private readonly IGroupUseCase _groupService;
         private int studentCount = 0;
-        private int groupSelectedIndex = 0;
+        private int groupID = 0;
         private IEnumerable<Student> students;
+        private List<Guid> selectedItemsGuids = new List<Guid>();
         public MainWindow(IGroupUseCase groupUseCase)
         {
             InitializeComponent();
@@ -24,36 +25,63 @@ namespace PresenceDesktop
         {
             InitializeComponent();
         }
+        private void Call()
+        {
+            students = _groupService.GetStudentsByGroupId(groupID);
+            StudentsListBox.ItemsSource = students;
+            count.Text = $"Всего: {students.Count()}";
+            SortComboBox.IsVisible = true;
+            SortComboBox.SelectedIndex = 0;
+            DeleteByGroupButton.IsVisible = true;
+        }
 
         private void GroupsComboBox_SelectionChanged(object? sender, Avalonia.Controls.SelectionChangedEventArgs e)
         {
             var comboBox = sender as ComboBox;
-            students = _groupService.GetStudentsByGroupId(comboBox.SelectedIndex + 1);
-            StudentsListBox.ItemsSource = students;
-            count.Text = $"Всего: {students.Count()}";
+            groupID = comboBox.SelectedIndex + 1;
+            Call();
         }
 
         private void MenuItem_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            //var menuItem = sender as MenuItem;
-            //Guid id = Guid.Parse(menuItem.Tag.ToString());
-            //_groupService.RemoveStudentById(id);
+            foreach (Guid guid in selectedItemsGuids)
+            {
+                _groupService.RemoveStudentById(guid);
+            }
+            Call();
         }
 
         private void SortComboBox_SelectionChanged(object? sender, Avalonia.Controls.SelectionChangedEventArgs e)
         {
-            //var comboBox = sender as ComboBox;
-            //switch (comboBox.SelectedIndex)
-            //{
-            //    case 0:
-            //        StudentsListBox.ItemsSource = students;
-            //        break;
-            //    case 1:
-            //        StudentsListBox.ItemsSource = students.OrderBy(Student.Name);
-            //        break;
-            //    case 2:
-            //        break;
-            //}
+            var comboBox = sender as ComboBox;
+            switch (comboBox.SelectedIndex)
+            {
+                case 0:
+                    StudentsListBox.ItemsSource = students;
+                    break;
+                case 1:
+                    StudentsListBox.ItemsSource = students.OrderBy(s => s.Name);
+                    break;
+                case 2:
+                    StudentsListBox.ItemsSource = students.OrderByDescending(s => s.Name);
+                    break;
+            }
+        }
+
+        private void DeleteByGroupButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            _groupService.RemoveStudentsByGroupId(groupID);
+            Call();
+        }
+
+        private void StudentsListBox_SelectionChanged(object? sender, Avalonia.Controls.SelectionChangedEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            foreach (var item in listBox.SelectedItems)
+            {
+                var student = item as Student;
+                selectedItemsGuids.Add(student.Guid);
+            }
         }
     }
 }
